b6e6a6f7269e4c12a52c95e38703a58d2309576c
ejderox112 <oxreklam35@gmail.com>
2025-10-24 23:23:04 +0300

    Create types/enums.ts

---
+++ types/enums.ts
@@ -0,0 +1,28 @@
+export enum UserRole {
+  Admin = 'admin',
+  Moderator = 'moderator',
+  Runner = 'runner',
+  User = 'user',
+}
+
+export enum SpaceType {
+  Hospital = 'hospital',
+  School = 'school',
+  Mall = 'mall',
+  Cafe = 'cafe',
+  Other = 'other',
+}
+
+export enum TaskStatus {
+  Pending = 'pending',
+  Completed = 'completed',
+  Rejected = 'rejected',
+}
+
+export enum ContentType {
+  Photo = 'photo',
+  Task = 'task',
+  Badge = 'badge',
+  Space = 'space',
+  Comment = 'comment',
+}
