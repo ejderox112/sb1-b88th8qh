7c91d44d6ec1028965c5d7da7739ff5764ff1346
ejderox112 <oxreklam35@gmail.com>
2025-10-24 23:47:58 +0300

    Create config/constants.ts

---
+++ config/config/constants.ts
@@ -0,0 +1,6 @@
+export const MAX_TASKS_PER_SPACE = 50;
+export const MAX_PHOTOS_PER_TASK = 5;
+export const TRUST_SCORE_THRESHOLD = 75;
+export const FLAG_LIMIT_BEFORE_REVIEW = 3;
+export const XP_PER_APPROVED_TASK = 20;
+export const XP_PER_PHOTO_UPLOAD = 5;
